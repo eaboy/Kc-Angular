fffb9e38fadbd37ad7f3719bf6a728265743b7cc
Eduardo <e.aboy@sinteac.es>
2017-09-26 00:37:48 +0200

    Añadimos los comentarios a nuestro AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+// Con el decoredor '@Component' otorgamos a la clase decorada comportamiento de componente
 @Component({
+  // El metadato 'selector' indica el selector css encargado de seleccionar el elemento HTML en el cual instanciar el componente
   selector: 'app-root',
+  // En el metadato 'templateUrl' indicamos el template (documento html) asociado al componente
   templateUrl: './app.component.html',
+  //  En el metadato 'styleUrls' indicamos las hojas de estilo que se aplican al componente
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
